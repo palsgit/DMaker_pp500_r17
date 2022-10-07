83e0fec08adf6e9ed03b068d535ad51cfb13eb3e
svobodamichal <svobodamichal@github.com>
2022-10-08 01:21:42 +0200

    Sti

--- StRoot/macros/runPicoD0AnaMakerLocal.C
+++ StRoot/macros/runPicoD0AnaMakerLocal.C
@@ -1,7 +1,7 @@
 #include "TROOT.h"
 #include "TSystem.h"
 #include "TChain.h"
-#include "StMaker.h"
+#include "StiMaker.h"
 #include "StChain.h"
 #include "StPicoDstMaker/StPicoDstMaker.h"
 #include "StPicoHFMaker/StPicoHFEvent.h"
